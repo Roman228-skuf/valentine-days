af3081953dbb7e2192264570aa1118299c9670a5
Roman228-skuf <24b_yurs@liceum.ztu.edu.ua>
2025-02-17 11:20:06 +0200

    Add wishes section and script for dynamic content

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log('Sript connected');
+
+document.getElementById('btn_wishes').addEventListener('click', function() {
+   // alert('Button clicked');
+    document.getElementById('p_wishes').innerText = 'Кохайте один одного';
+});
